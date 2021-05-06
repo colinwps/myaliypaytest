4f682b025ea680a025f6dbf973d2ec605f13f068
colinwps <colinapp@hotmail.com>
2021-05-07 00:14:29 +0800

    Update AlipayInfo.java
    测试购买页

--- src/main/java/com/colinapp/myaliypaytest/controller/AlipayInfo.java
+++ src/main/java/com/colinapp/myaliypaytest/controller/AlipayInfo.java
@@ -1,8 +1,15 @@
 package com.colinapp.myaliypaytest.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AlipayInfo {
+
+
+    @GetMapping("buy")
+    public  String BayPage(){
+        return "BayInfo";
+    }
 
 }
