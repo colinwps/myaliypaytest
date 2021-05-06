946255bccecf0e2c9f861e0896ad8143d887a342
admin <admin@ADMIN-PC>
2021-05-06 17:57:47 +0800

    Initial commit

---
+++ src/main/java/com/colinapp/myaliypaytest/MyaliypaytestApplication.java
@@ -0,0 +1,13 @@
+package com.colinapp.myaliypaytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyaliypaytestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyaliypaytestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/colinapp/myaliypaytest/MyaliypaytestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colinapp.myaliypaytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyaliypaytestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
